842350819f218cdb1c464ee97f4ab04b7b9fd9cb
LiangL2020 <qiuyinglu2012@gmail.com>
2023-01-15 23:28:40 -0500

    Initial Commit

---
+++ Kalculata/KalculataApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalculataApp.swift
+//  Kalculata
+//
+//  Created by Liang Lu on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct KalculataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
